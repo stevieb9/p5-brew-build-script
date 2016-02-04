705f55bbcec470cd1c645a7406e6e083459ae604
Steve Bertrand <steveb@cpan.org>
2016-02-04 13:58:25 +0000

    added opts, working on nix

--- brew_build.pl
+++ brew_build.pl
@@ -3,19 +3,41 @@ use warnings;
 use strict;
 
 use Cwd;
+use Getopt::Long;
 
-# set $debug to true to print out running info
+my ($debug, $count, $reload, $version, $help);
 
-my $debug = 1;
+GetOptions(
+    "debug=i" => \$debug,
+    "count=i" => \$count,
+    "reload=i" => \$reload,
+    "version=s" => \$version,
+    "help"      => \$help,
+);
 
-print "\nUsage: build.pl run_count [reload]\n" if ! defined $ARGV[0];
+if ($help){
+    print <<EOF;
+    
+    Usage: perl build/brewbuild.pl [options]
 
-my $num = $ARGV[0];
-my $reload = $ARGV[1];
+    Options:
 
-my $is_win = 0;
+    --debug | -d:   Bool, enable verbosity
+    --count | -c:   Integer, how many random versions of perl to install
+    --reload | -r:  Bool, remove all installed perls (less the current one)
+                    before installation of new ones
+    --verion | -v:  String (N.NN.N) the number portion of an available
+                    perl version according to "perlbrew available" Note
+                    that only one is allowed at this time
+    --help | -h:    print this message
+EOF
+exit;
+}
 
-run($num);
+my $cwd = getcwd();
+my $is_win = $^O =~ /Win/ ? 1 : 0;
+
+run($count);
 
 sub perls_available {
     my $brew_info = shift;
@@ -30,9 +52,11 @@ sub perls_installed {
 
     return $is_win
         ? $brew_info =~ /(\d\.\d{2}\.\d(?:_\d{2}))(?!=_)\s+\[installed\]/ig
-        : $brew_info =~ /(perl-\d\.\d+\.\d+)/g;
+        : $brew_info =~ /i.*?(perl-\d\.\d+\.\d+)/g;
 }
 sub instance_remove {
+    my @perls_installed = @_;
+
     print "\nremoving previous installs...\n" if $debug;
 
     my $remove_cmd = $is_win
@@ -43,31 +67,39 @@ sub instance_remove {
         my $ver = $^V;
         $ver =~ s/v//;
 
-        print "skipping version we're using, $_\n" if $debug;
-        next if $_ =~ /$ver$/;
-
+        if ($_ =~ /$ver$/){
+            print "skipping version we're using, $_\n" if $debug;
+            next;
+        }
         `$remove_cmd $_`;
     }
 
     print "\nremoval of existing perl installs complete...\n" if $debug;
 }
 sub instance_install {
-    
     my $count = shift;
+    my @perls_available = @_;    
 
     my $install_cmd = $is_win
-        : 'berrybrew install'
-        ? 'perlbrew install --notest -j 4';
+        ? 'berrybrew install'
+        : 'perlbrew install --notest -j 4';
 
     my @new_installs;
 
-    for ($num){
-        push @new_installs, $perls_available[rand @perls_available];
+    if ($version){
+        push @new_installs, "perl-$version";
+    }
+    else {
+        for ($count){
+            push @new_installs, $perls_available[rand @perls_available];
+        }
     }
 
-    for (@new_installs){
-        print "\ninstalling $_...\n" if $debug;
-        `$install_cmd $_`;
+    if ($count){
+        for (@new_installs){
+            print "\ninstalling $_...\n" if $debug;
+            `$install_cmd $_`;
+        }
     }
     else {
         print "\nusing existing versions only\n" if $debug;
@@ -76,12 +108,12 @@ sub instance_install {
 sub results {
 
     my $exec_cmd = $is_win
-        ? 'berrybrew exec perl build\\test.pl'
-        : 'perlbrew exec build/test.pl';
+        ? "berrybrew exec perl $cwd\\build\\test.pl"
+        : "perlbrew exec perl $cwd/build/test.pl 2>/dev/null";
 
     my $debug_exec_cmd = $is_win
-        ? 'berrybrew exec perl build\\test.pl'
-        : 'perlbrew exec build/test.pl 2>/dev/null';
+        ? "berrybrew exec perl $cwd\\build\\test.pl"
+        : "perlbrew exec perl $cwd/build/test.pl";
 
     my $result;
 
@@ -112,25 +144,24 @@ sub results {
         }
         my $res;
         if (/Result:\s+(PASS)/){
-            print $_;
             $res = $1; 
         }
         else {
+            print $_;
             $res = 'FAIL';
         }
         print "$ver :: $res\n";
     }
 }
-
 sub run {
 
-    my $num = shift;
+    my $count = shift // 0;
 
     my $brew_info = `perlbrew available`;
     
     my @perls_available = perls_available($brew_info);
 
-    $num = scalar @perls_available if $num =~ /all/;
+    $count = scalar @perls_available if $count -= -1;
 
     my @perls_installed = perls_installed($brew_info);
 
@@ -141,8 +172,8 @@ sub run {
 
     my %perl_vers;
 
-    instance_remove() if $reload;
-    instance_install($num);
+    instance_remove(@perls_installed) if $reload;
+    instance_install($count, @perls_available);
 
     results();
 }
